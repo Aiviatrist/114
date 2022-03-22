cb2cce1e71c696ebb7d70ab7d02fe84a42b75b4d
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-03-22 19:29:18 +1000

    Add files via upload

--- java.js
+++ java.js
@@ -1,6 +1,22 @@
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded());
+    posenet.on("pose",gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function modelloaded(){
+    console.log("moddel is loaded")
+}
+function draw(){
+    Image(video,0,0,300,300);
 }
 function pic(){
     save("filterpic.png");
